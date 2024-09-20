3e35bec1ac562c8bfd58185f84d6b6c493d4d065
shuckiy73 <121363089+shuckiy73@users.noreply.github.com>
2024-09-20 08:32:34 +0300

    modified file

--- lesson2/lesson1/parser/services.py
+++ lesson2/lesson1/parser/services.py
@@ -8,16 +8,20 @@ class KufarService:
 
     def search_by_word(self, word):
         try:
+            self.data_client_imp.connect()  # Открываем соединение с базой данных
             results = self.data_client_imp.select_by_word(word)
             for el in results:
                 logging.info(el)
         except Exception as e:
             logging.error(f"Ошибка при поиске по слову '{word}': {e}")
+        finally:
+            self.data_client_imp.close()  # Закрываем соединение с базой данных
 
     def search_by_word_and_price(self, word, price_from, price_to):
         try:
             if price_from > price_to:
                 raise ValueError("Значение 'price_from' должно быть меньше или равно 'price_to'")
+            self.data_client_imp.connect()  # Открываем соединение с базой данных
             results = self.data_client_imp.select_by_word_and_price(word, price_from, price_to)
             for el in results:
                 logging.info(el)
@@ -25,6 +29,8 @@ class KufarService:
             logging.error(f"Ошибка входных данных: {ve}")
         except Exception as e:
             logging.error(f"Ошибка при поиске по слову '{word}' и цене от {price_from} до {price_to}: {e}")
+        finally:
+            self.data_client_imp.close()  # Закрываем соединение с базой данных
 
 # Пример использования
 # KufarService().search_by_word('Минск')
